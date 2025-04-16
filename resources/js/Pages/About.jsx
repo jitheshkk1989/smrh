4336dc6cb397fbf63c879e6300643942d7a2d37c
Jithesh Kk <jitheshkk1989@gmail.com>
2025-04-16 14:26:55 +0530

    New Chnages

---
+++ resources/js/Pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>About Page</h1>
+      <p>This is a new route using Laravel 10 + Inertia + React!</p>
+    </div>
+  );
+}
