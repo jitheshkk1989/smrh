43dbcde1b7156270f1eff026f72a4f00bbdaec46
Jithesh Kk <jitheshkk1989@gmail.com>
2025-04-16 14:14:22 +0530

    Inertia Commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Home({ name }) {
+  return <h1>Welcome, {name}!</h1>
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use Inertia\Inertia;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', ['name' => 'Inertia + React']);
 });
